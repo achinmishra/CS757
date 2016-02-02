243c8664aceaa3909a0745c6354a78d13e14afa7
Wshaikh <wasimrs@gmail.com>
2016-02-01 19:05:58 -0600

    pragmas added for omp_ocean.c

--- omp_ocean.c
+++ omp_ocean.c
@@ -16,5 +16,24 @@ void ocean (int **grid[2], int xdim, int ydim, int timesteps)
      * the other grid. Then, swap the grids for the next
      * iteration.
      ******************************************************/
+	int i,j,temp;
+	int **temp_grid;
 
+	while (timesteps-- >= 0)
+	{
+	#pragma omp parallel for shared(grid,temp_grid, xdim, ydim) \
+private(i,j) schedule(dynamic)	
+		for (i = 1; i < xdim - 1; i++)
+		{
+			for (j = 1; j < ydim - 1 ; j++)
+			{
+				//calculating the value and storing it in the new grid
+				grid[1][i][j] = 0.2 * (grid[0][i][j] + grid[0][i][j-1] + grid[0][i-1][j] + grid[0][i][j+1] + grid[0][i+1][j]);
+			}
+		}
+		//swap the grid now
+		temp_grid = grid[0];
+		grid[0] = grid[1];
+		grid[1] = temp_grid;
+	}
 }
